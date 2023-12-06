7fb51ceb96f3a7924d5297c837fbee2bb1b41499
jwobeto <joaobwobeto@hotmail.com>
2023-12-05 23:28:29 -0300

    inclusão do atributo preço

--- apps/users/models.py
+++ apps/users/models.py
@@ -12,6 +12,7 @@ class Users(models.Model):
     email = models.CharField('Email', max_length=50, unique=True)
     password = models.CharField('Senha', max_length=128,null=False,default=phone)
     profile = models.CharField(max_length=1, choices=PROFILE_CHOICES, blank=False, null=False,default="c")
+    
     class Meta:
         verbose_name = 'Users'
         verbose_name_plural = 'Users'
